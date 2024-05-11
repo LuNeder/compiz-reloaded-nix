c932949b10ba0be01fd9e682f90fb1859f980538
Luana <luana@luana.dev.br>
2024-05-11 18:44:34 -0300

    wip
    i wonder if it's really just this

--- default.nix
+++ default.nix
@@ -80,19 +80,12 @@ stdenv.mkDerivation (f: {
   patches = [
   
   ];
-
-  cmakeFlags = [
-    "-DCMAKE_CXX_STANDARD=17"
-    "-DCMAKE_BUILD_TYPE='Release'"
-    "-DCOMPIZ_DISABLE_SCHEMAS_INSTALL=ON"
-    "-DCOMPIZ_BUILD_WITH_RPATH=OFF"
-    "-DCOMPIZ_PACKAGING_ENABLED=ON"
-    "-DBUILD_GTK=ON"
-    "-DBUILD_METACITY=ON"
-    "-DBUILD_KDE4=OFF"
-    "-DCOMPIZ_DEFAULT_PLUGINS='composite,opengl,decor,resize,place,move,compiztoolbox,staticswitcher,regex,animation,wall,ccp'"
-    "-DCOMPIZ_BUILD_TESTING=OFF"
-    "-DCOMPIZ_WERROR=OFF"
-    "-Wno-dev"
+  
+  configureFlags = [
+    "--disable-static"
+    "--enable-marco"
+    "--with-gtk=3.0"
+    "--with-default-plugins=core,ccp,decoration,dbus,commands,ezoom,fade,minimize,mousepoll,move,place,png,regex,resize,session,snap,switcher,vpswitch,wall,workarounds,matecompat"
   ];
+
 })
