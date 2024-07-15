c817fb6aa6aefd76ace25b48782ee3af65179945
Luana <luana@luana.dev.br>
2024-07-15 15:03:39 -0300

    Actually fixes now (hopefully)
    Fix from https://github.com/NixOS/nixpkgs/pull/325773/files (and Natan from GELOS)

--- flake.nix
+++ flake.nix
@@ -12,14 +12,14 @@
     forEachSystem = nixpkgs.lib.genAttrs (import systems);
   in {
     overlays.default = final: prev: {
-      compiz = final.callPackage ./. {};
+      compiz = final.callPackage ./. { python3 = pkgs.python311; python3Packages = pkgs.python311Packages; };
     };
 
     packages = forEachSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in rec {
       default = compiz;
-      compiz = pkgs.callPackage ./. {};
+      compiz = pkgs.callPackage ./. { python3 = pkgs.python311; python3Packages = pkgs.python311Packages; };
     });
   };
 }
